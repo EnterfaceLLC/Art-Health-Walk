07f316d2d0ac4a0e871d2dcb90802b5501408c1c
Rogelio Zamora Jr <dotenterface@gmail.com>
2023-04-25 22:02:09 -0500

    folder structure; theme folder - palette, system colors; +logo

---
+++ src/theme/colors.js
@@ -0,0 +1,33 @@
+export const palette = {
+
+  primary: '#751D62', //* Disco Dark
+
+  secondary: '#A41E5E', //* Disco Light 
+
+  tertiary: '#D51A66', //* Red Violet
+
+  accent1: '##24B2C0', //* Java
+
+  accent2: '##BCD531', //* Key Lime Pie
+
+  accent3: '#19B19A', //* Keppel
+
+  accent4: '##ed8e22', //* Carrot Orange
+
+};
+
+export const system = {
+
+  black: '#000',  //* Black
+
+  white: '#FFF', //* White
+
+  grey: '#E0E0E0', //* Platinum
+
+  success: '#78C091', //* Mint Green
+
+  error: '#DE3C4B', //* Rusty Red
+
+  warning: '#F9C22E', //* Saffron
+
+};
